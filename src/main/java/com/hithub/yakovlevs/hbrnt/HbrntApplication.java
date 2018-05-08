4930d96eccdc01ee159fd231e758461ba3e1449a
syakovlev <jakovlev.s.a@yandex.ru>
2018-05-08 08:54:11 +0300

    Initial commit.

---
+++ src/main/java/com/hithub/yakovlevs/hbrnt/HbrntApplication.java
@@ -0,0 +1,12 @@
+package com.hithub.yakovlevs.hbrnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbrntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HbrntApplication.class, args);
+	}
+}
